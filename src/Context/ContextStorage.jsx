03bf1dd9b3813d125a0e63765532f67a8dd20790
Renan <renan13.com@gmailcom>
2025-03-04 08:17:07 -0300

    logica de pegar dados na memoria usando useEffect

--- src/Context/ContextStorage.jsx
+++ src/Context/ContextStorage.jsx
@@ -1,13 +1,21 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const ContextStorage = createContext()
+export const ContextStorage = createContext();
 
 export const StorageProvider = ({ children }) => {
+  const [dataStorage, setDataStorage] = useState(null);
+
+  useEffect(() => {
+    const StorageData = localStorage.getItem("@dataStorage");
+    if (StorageData) {
+      setDataStorage(JSON.parse(StorageData));
+    }
+  }, []);
     
- 
-    return (
-        <ContextStorage.Provider value={{teste}}>
-            {children}
-        </ContextStorage.Provider>
-    )
-}
+
+  return (
+    <ContextStorage.Provider value={{ dataStorage }}>
+      {children}
+    </ContextStorage.Provider>
+  );
+};
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,23 @@
 // import styles from './Home.module.css'
 
+import { useContext } from "react";
 import Head from "../Components/Head";
+import { ContextStorage } from "../Context/ContextStorage";
 
-const Home  = () => {
+const Home = () => {
+    const { dataStorage } = useContext(ContextStorage)
+    console.log();
+    
 return (
     <div>
         <Head/>
-        <p>Home</p>
+        <div>
+            {
+                dataStorage ? <div>
+                    tem itens 
+                </div> : <p>aindao a tarefas a serem mostradas</p>
+            }
+        </div>
     </div>
 )}
 
