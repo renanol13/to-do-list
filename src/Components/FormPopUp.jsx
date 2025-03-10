4c8583f9624e2f8d02b97353ac7ac692c585bfd7
Renan <renan13.com@gmailcom>
2025-03-10 20:28:02 -0300

    criando efeito de desfoque na tela de adicionar tarefa

--- src/Components/FormPopUp.jsx
+++ src/Components/FormPopUp.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import styles from "./FormPopUp.module.css";
 
 import { FaShareAlt } from "react-icons/fa";
+import Conteiner from "../layout/Conteiner";
 
 const FormPopUp = ({ onclose }) => {
   const boxInputRef = useRef(null);
@@ -12,31 +13,31 @@ const FormPopUp = ({ onclose }) => {
   }, []);
 
   return (
-    <div className={styles.boxFormPopUp} onClick={() => onclose()}>
-      <div className={styles.boxForm} onClick={(e) => e.stopPropagation()}>
-        <label htmlFor="textTask"></label>
-        <textarea
-          id="textTask"
-          ref={boxInputRef}
-          name="textTask"
-          placeholder="Insira uma nova tarefa aqui"
-        ></textarea>
-        <div className={styles.boxMoreOptions}>
-          <label htmlFor="category"></label>
-          <select name="category" id="category">
-            <option value="" disabled>
-              Sem categoria
-            </option>
-            <option value="trabalho">Trabalho</option>
-            <option value="pessoal">Pessoal</option>
-            <option value="listaDeDesejos">Lista de Desejos</option>
-          </select>
-          <label htmlFor="dateTask"></label>
-          <input type="date" name="dateTask" id="dateTask" />
-          <FaShareAlt />
+      <div className={styles.boxFormPopUp} onClick={() => onclose()}>
+        <div className={styles.boxForm} onClick={(e) => e.stopPropagation()}>
+          <label htmlFor="textTask"></label>
+          <textarea
+            id="textTask"
+            ref={boxInputRef}
+            name="textTask"
+            placeholder="Insira uma nova tarefa aqui"
+          ></textarea>
+          <div className={styles.boxMoreOptions}>
+            <label htmlFor="category"></label>
+            <select name="category" id="category">
+              <option value="" disabled>
+                Sem categoria
+              </option>
+              <option value="trabalho">Trabalho</option>
+              <option value="pessoal">Pessoal</option>
+              <option value="listaDeDesejos">Lista de Desejos</option>
+            </select>
+            <label htmlFor="dateTask"></label>
+            <input type="date" name="dateTask" id="dateTask" />
+            <FaShareAlt />
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
