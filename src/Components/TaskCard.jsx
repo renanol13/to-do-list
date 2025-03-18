df8a48af67a80d5c4db47ab9e58598fa3d348052
Renan <renan13.com@gmailcom>
2025-03-17 21:35:08 -0300

    Criando componente e efeitos do card Functions

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -2,10 +2,12 @@ import styles from "./TaskCard.module.css";
 import { gsap } from "gsap";
 import { FaCheck } from "react-icons/fa";
 import { useLayoutEffect, useRef, useState } from "react";
+import TaskCardFunctions from "./TaskCardFunctions";
 
 const TaskCard = () => {
   const boxTaskRef = useRef(null);
   const boxIconCompleteTaskRef = useRef(null);
+  const [activeFunctionsCard, setActiveFunctionsCard] = useState(false);
   const [activeCompleteTask, setActiveCompleteTask] = useState(false);
   const tl = gsap.timeline();
 
@@ -29,21 +31,37 @@ const TaskCard = () => {
       gsap.killTweensOf(boxTaskRef.current, boxIconCompleteTaskRef.current);
   }, [activeCompleteTask]);
 
+  const openFunctionsCard = () => {
+    if (!activeFunctionsCard) {
+      setTimeout(() => setActiveFunctionsCard(true), 1000);
+    }
+  };
+
   return (
-    <div ref={boxTaskRef} className={styles.boxTaskCard}>
-      <div
-        onClick={() => setActiveCompleteTask(!activeCompleteTask)}
-        className={styles.boxIconCompleteTask}
-      >
-        <FaCheck ref={boxIconCompleteTaskRef} />
-      </div>
-      <div className={styles.boxInfo}>
-        <h4 className={styles.content}>Isso é minha primeira tarefa</h4>
-        <div className={styles.boxTimes}>
-          <p id="date">12-11-2025</p>
-          <p id="hours">12-45</p>
-        </div>
-      </div>
+    <div
+      ref={boxTaskRef}
+      className={styles.boxTaskCard}
+      onMouseDown={openFunctionsCard}
+    >
+      {activeFunctionsCard ? (
+        <TaskCardFunctions />
+      ) : (
+        <>
+          <div
+            onClick={() => setActiveCompleteTask(!activeCompleteTask)}
+            className={styles.boxIconCompleteTask}
+          >
+            <FaCheck ref={boxIconCompleteTaskRef} />
+          </div>
+          <div className={styles.boxInfo}>
+            <h4 className={styles.content}>Isso é minha primeira tarefa</h4>
+            <div className={styles.boxTimes}>
+              <p id="date">12-11-2025</p>
+              <p id="hours">12-45</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/Components/TaskCardFunctions.jsx
@@ -0,0 +1,36 @@
+import styles from "./TaskCardFunctions.module.css";
+import { RiDeleteBinFill } from "react-icons/ri";
+import { FaShareAlt } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+import { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+const TaskCardFunctions = () => {
+  const activeBoxFunctions = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.to(activeBoxFunctions.current, {
+      opacity: 1,
+      duration: 0.1,
+      scale: 1,
+    });
+  }, []);
+
+  return (
+    <div className={styles.boxTaskCardFunctions} ref={activeBoxFunctions}>
+      <button className={styles.share}>
+        <FaShareAlt />
+      </button>
+      <button className={styles.edit}>
+        <MdEdit />
+        <p>Editar</p>
+      </button>
+      <button className={styles.del}>
+        <RiDeleteBinFill />
+        <p>Deletar</p>
+      </button>
+    </div>
+  );
+};
+
+export default TaskCardFunctions;
